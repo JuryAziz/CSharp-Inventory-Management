01330e40a11afa1e8066d236f00922aef4968d10
Jury Alharbi <jury.abdulaziz@gmail.com>
2024-03-25 23:48:56 +0300

    Basic -not tested-

--- Program.cs
+++ Program.cs
@@ -1,1 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Dynamic;
+
+namespace InventoryManagement
+{
+
+    public class Item
+    {
+
+        private readonly string _name;
+        private int _quantity;
+        private DateTime _dateCreated;
+
+        Item(string name, int quantity, DateTime? date = null)
+        {
+            _name = name;
+            _quantity = quantity >= 0 ? quantity : throw new Exception("Invalid. quantity cannot be negative!");
+            _dateCreated = date ?? DateTime.Now;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+
+    public class Store
+    {
+
+        private List<Item> _items = new List<Item>();
+
+        bool AddItem(Item item)
+        {
+            if (FindItemByName(item.GetName()) == null) return false;
+            _items.Add(item);
+            return true;
+        }
+
+        bool RemoveItem(Item item)
+        {
+            if (FindItemByName(item.GetName()) == null) return false;
+            _items.Remove(item);
+            return true;
+        }
+
+
+        Item FindItemByName(string name)
+        {
+            return _items.Find(item => item.GetName().ToUpper().Equals(name.ToUpper()));
+        }
+
+        void SortByNameAsc()
+        {
+            _items.OrderBy(item => item.GetName().ToUpper());
+        }
+    }
+}
